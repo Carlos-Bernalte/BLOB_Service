a672ea804d948e73ce493a57f42de31bb414c8fe
Angel Garcia <72787323+theangelogarci@users.noreply.github.com>
2022-11-05 00:32:33 +0100

    Added Timeouts and 200 code control

--- client.py
+++ client.py
@@ -14,6 +14,14 @@ headersList2 = {
  "admin token": "1234"
 }
 
+class RestListError(Exception):
+    '''Error caused by wrong responses from server'''
+    def __init__(self, message='unknown'):
+        self.msg = message
+
+    def __str__(self):
+        return f'RestListError: {self.msg}'
+        
 class BlobService:
     '''Cliente de acceso al servicio de blobbing'''
     def __init__(self,uri,timeout=120):
@@ -24,17 +32,29 @@ class BlobService:
         '''Crea un nuevo blob usando el usuario establecido'''
         payload = json.dumps({"path": "nombre.txt"})
         response = requests.request("PUT", self.uri+'/v1/blob', data=payload,  headers=headersList1)
-        print (response.text)
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
     def get_blob(self, blob_id, user):
         '''Obtiene un blob usando el usuario indicado'''
         response = requests.request("GET", self.uri+'/v1/blob' ,  headers=headersList1)
-        print (response.text)
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
     def remove_blob(self, blob_id, user):
         '''Intenta eliminar un blob usando el usuario dado'''
         response = requests.request("DELETE", self.uri+'/v1/blob' ,  headers=headersList1)
-        print (response.text)
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
 
 class Blob:
@@ -49,43 +69,67 @@ class Blob:
     @property
     def is_online(self):
         '''Comprueba si el blob existe'''
+        response = None
         if self==None:
             return False
         else:
-            if (response = requests.request("GET", self.uri+'/v1/blob' ,  headers=headersList1)==200):
+            if (requests.request("GET", self.uri+'/v1/blob' ,  headers=headersList1)==200):
+                timeout=self.timeout
                 return True
 
     def dump_to(self, local_filename):
         '''Vuelca los datos del blob en un archivo local'''
         response = requests.request("GET", self.uri+'/v1/blob' ,  headers=headersList1)
-        getBlob = requests.get(self.uri+'/v1/blob/<int:blob_id>')
+        getBlob = requests.get(self.uri+'/v1/blob/<int:blob_id>') 
+        timeout=self.timeout
         open("/dumps/<int:blob_id>", "wb").write(getBlob.content)
 
     def refresh_from(self, local_filename):
         '''Reemplaza el blob por el contenido del fichero local'''
         response = requests.request("PUT", self.uri+'/v1/blob/<int:blob_id>',  headers=headersList1)
         refreshFrom= requests.put(self.uri+'/v1/blob/<int:blob_id>', data=open("/dumps/<int:blob_id>", "rb"))
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
     def add_read_permission_to(self, user):
         '''Permite al usuario dado leer el blob'''
         response = requests.request("PUT", self.uri+'/v1/blob/<int:blob_id>/readable_by/',  headers=headersList1)
-        print (response.text)
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
     def revoke_read_permission_to(self, user):
         '''Elimina al usuario dado de la lista de permiso de lectura'''
         response= requests.delete(self.uri+'/v1/blob/<int:blob_id>/readable_by/',  headers=headersList1)
-        print (response.text)
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
     def add_write_permission_to(self, user):
         '''Permite al usuario dado escribir el blob'''
         response= requests.put(self.uri+'/v1/blob/<int:blob_id>/writable_by/', headers=headersList1)
-        print (response.text)
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
 
     def revoke_write_permission_to(self, user):
         '''Elimina al usuario dado de la lista de permiso de escritura'''
         response= requests.delete(self.uri+'/v1/blob/<int:blob_id>/writable_by/', headers=headersList1)
-        print (response.text)
+        timeout=self.timeout
+        if response.status_code != 200:
+            raise RestListError(f'Unexpected status code: {response.status_code}')
+        else:
+            print (response.text)
 
 
 
