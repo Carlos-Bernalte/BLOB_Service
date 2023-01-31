8220b55a58dd661074477b4c4c6a5af884e30c09
Carlos-Bernalte <carlos.bernalte@alu.uclm.es>
2023-01-31 02:11:42 +0100

    mas cosas

--- deploy.py
+++ deploy.py
@@ -1,62 +1,19 @@
-"""
-Shows the functionality of exec using a Busybox container.
-"""
-
-import time
-
-from kubernetes import config
-from kubernetes.client import Configuration
-from kubernetes.client.api import core_v1_api
-from kubernetes.client.rest import ApiException
-from kubernetes.stream import stream
-
-
-def exec_commands(api_instance):
-    name = 'blob-pod'
-    resp = None
-    try:
-        resp = api_instance.read_namespaced_pod(name=name, namespace='default')
-    except ApiException as e:
-        if e.status != 404:
-            print("Unknown error: %s" % e)
-            exit(1)
-
-    if not resp:
-        print("Pod %s does not exist. Creating it..." % name)
-        pod_manifest = {
-            'apiVersion': 'v1',
-            'kind': 'Pod',
-            'metadata': {
-                'name': name
-            },
-            'spec': {
-                'containers': [{
-                    'image': 'theangelogarci/miapp',
-                    'name': 'blob'
-                }]
-            }
-        }
-        resp = api_instance.create_namespaced_pod(body=pod_manifest, namespace='default')
-        while True:
-            resp = api_instance.read_namespaced_pod(name=name, namespace='default')
-            if resp.status.phase != 'Pending':
-                break
-            time.sleep(1)
-        print("Done.")
-
+from kubernetes import client,config,utils
+import argparse
 
 def main():
     config.load_kube_config()
-    try:
-        c = Configuration().get_default_copy()
-    except AttributeError:
-        c = Configuration()
-        c.assert_hostname = False
-    Configuration.set_default(c)
-    core_v1 = core_v1_api.CoreV1Api()
+    k8s_client = client.ApiClient()
+    yaml_file = 'deployment.yaml'
+    utils.create_from_yaml(k8s_client,yaml_file,verbose=True)
 
-    exec_commands(core_v1)
+if __name__ == "__main__":
 
+    # argparser = argparse.ArgumentParser()
+    # argparser.add_argument('--host', type=str, required=True)
+    # try:
+    #     open(argparser.config)
+    # except FileNotFoundError:
+    #     print('File not found')
 
-if __name__ == '__main__':
     main()
